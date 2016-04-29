97081ae0a8f69ae36379275ccf7055b9ed3e262c
Kim <893235692@qq.com>
2016-04-29 23:20:44 +0800

    第一次提交

---
+++ MainActivity.java
@@ -0,0 +1,15 @@
+package com.a520it.mygit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("第一次");
+
+    }
+}
